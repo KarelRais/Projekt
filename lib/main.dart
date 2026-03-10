769ab18007ec05e5058aad599004dc738b8b6ab4
Karel Rais <rais_karel@oauh.cz>
2026-03-10 08:45:52 +0100

    Průběžné uložení

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:file_picker/file_picker.dart';
+import 'bluetooth_service.dart';
+import 'filesystem_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,14 +36,38 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    String storedString = "";
+    final _savedDelta = jsonDecode(storedString);
+    final _controller = QuillController(
+      document: Document.fromJson(_savedDelta),
+      selection: const TextSelection.collapsed(offset: 0),
+    );
+    storedString = jsonEncode(_controller.document.toDelta().toJson());
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          
+        ]
       ),
       body: Center(
-        child: Column(mainAxisAlignment: .center, children: [
-            
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            QuillToolbar.basic(controller: _controller),
+            Expanded(
+              child: QuillEditor(
+                controller: _controller,
+                scrollController: ScrollController(),
+                scrollable: true,
+                focusNode: FocusNode(),
+                autoFocus: true,
+                readOnly: false,
+                expands: true,
+                padding: EdgeInsets.all(8),
+              ),
+            ),
           ],
         ),
       ),
